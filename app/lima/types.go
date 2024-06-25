69dc7dad06f6f08ec8bc90255f8b2a5fd2f1e92b
Ranjandas <thejranjan@gmail.com>
2024-06-25 12:56:45 +1000

    Add ImagePath to List Output

--- app/lima/types.go
+++ app/lima/types.go
@@ -1,9 +1,15 @@
 package lima
 
+type Config struct {
+	Env    map[string]string   `json:"env"`
+	Images []map[string]string `json:"images"`
+}
+
 type LimaVM struct {
 	Name   string `json:"name"`
 	Status string `json:"status"`
 	Memory uint64 `json:"memory"`
 	Disk   uint64 `json:"disk"`
 	Cpus   int    `json:"cpus"`
+	Config Config `json:"config"`
 }
