c29c04d22c7b12352e5873830863eda1c26a0849
Ranjandas <thejranjan@gmail.com>
2024-06-27 07:50:51 +1000

    Fixes version command help text (#40)

--- cmd/version.go
+++ cmd/version.go
@@ -14,13 +14,8 @@ var Version string
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints the version of Shikari",
+	Long:  `Prints the version of Shikari`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(Version)
 	},
